d436c91b289a3df7cc9844ef8f16d3dc07809415
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-06-22 20:19:20 -0300

    feat: implementando o swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,13 @@ import { Controller, Post, Body } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService
+    private readonly jwtService: JwtService
+  ) {}
 
   @Post('login')
   login(@Body() loginDto: LoginDto) {
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
 // Implementando o Login
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   senha: string;
 }
