41953a55863cabdd5120f951bcef119a5cec1e53
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-06-22 19:43:04 -0300

    feat: implementando a validação de login

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,11 @@
+// Implementando o Login
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
